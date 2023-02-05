8181c4a4280c546761c597a260f114abf6205b46
Ryan Dopler <rddopler@gmail.com>
2023-02-05 04:01:17 -0500

    Initial commit

---
+++ src/main/java/com/dopler/zeusresource/ZeusResourceApplication.java
@@ -0,0 +1,13 @@
+package com.dopler.zeusresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusResourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dopler/zeusresource/ZeusResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dopler.zeusresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
